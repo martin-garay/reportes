4584d7c2ec33f6fe3d68e17875735bbdd2294848
laacademia <martingaray_12@hotmail.com>
2020-07-02 14:33:38 -0300

    Reportes para version toba 2.7

--- metadatos/tablas/apex_fuente_datos_schemas.sql
+++ metadatos/tablas/apex_fuente_datos_schemas.sql
@@ -10,6 +10,12 @@ INSERT INTO apex_fuente_datos_schemas (proyecto, fuente_datos, nombre, principal
 );
 INSERT INTO apex_fuente_datos_schemas (proyecto, fuente_datos, nombre, principal) VALUES (
 	'reportes', --proyecto
+	'negocio', --fuente_datos
+	'sistema', --nombre
+	'0'  --principal
+);
+INSERT INTO apex_fuente_datos_schemas (proyecto, fuente_datos, nombre, principal) VALUES (
+	'reportes', --proyecto
 	'reportes', --fuente_datos
 	'public', --nombre
 	'0'  --principal
